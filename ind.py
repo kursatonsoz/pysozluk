f642bb18b8bbc4156925090cc557dc4da8b8147d
Kürşat Önsöz <kursat@kursat-GE60-0NC-GE60-0ND.(none)>
2014-01-07 13:50:59 +0200

    Message

---
+++ ind.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+import pynotify
+import os
+pynotify.init('PySozluk')
+file = open("kelime.txt","r")
+
+for kel in file:
+	bir = kel.split("=")[0]
+	iki = kel.split("=")[1]
+	n = pynotify.Notification('%s'%bir,'%s'%iki)
+	n.set_timeout(0)
+	n.show()
+	os.system("sleep 5")
